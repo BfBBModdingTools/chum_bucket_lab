b9c015f3c77c8ef9270b66fd1a2487669371539c
Steven Casper <sebastiancasper3@gmail.com>
2021-04-09 01:52:16 -0400

    mods.json Automtatic updates
    The program will now retrieve the lastest version of mods.json from master and
    overwrite the existing file.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,28 @@ use druid::{im::Vector, AppLauncher, LocalizedString, WindowDesc};
 
 use data::{AppState, Mod};
 use std::fs;
+use std::io::Write;
 
 pub fn main() {
     let main_window = WindowDesc::new(ui::ui_builder)
         .title(LocalizedString::new("bfbb_modloader").with_placeholder("BfBB Modloader"));
+
+    if let Ok(response) = reqwest::blocking::get(
+        "https://raw.githubusercontent.com/SquareMan/bfbb_modloader/master/mods.json",
+    ) {
+        if let Ok(modlist_json) = response.text() {
+            if let Ok(mut file) = fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .open("mods.json")
+            {
+                match file.write_all(modlist_json.as_bytes()) {
+                    Ok(_) => (),
+                    Err(_) => (),
+                }
+            }
+        }
+    }
 
     // FIXME: More robust file I/O, any file error will currently result in a panic
     let modlist: Vec<Mod> = serde_json::from_reader(fs::File::open("mods.json").unwrap()).unwrap();
