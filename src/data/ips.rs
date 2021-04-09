daa6d964e4c0db33620cf33a877cdc91aefa93f2
Steven Casper <sebastiancasper3@gmail.com>
2021-04-09 07:45:12 -0400

    Implement RLE encoding and fix minor bugs
    Correctly insert bytes that are placed after the end of the original
    ROM in the correct place.
    Fix iterator length for Non RLE decoder

--- src/data/ips.rs
+++ src/data/ips.rs
@@ -24,6 +24,7 @@ impl Ips {
         f.set_position(5);
 
         // NOTE: 0th index is padding and should always be ZERO
+        let rom_start_size = rom.len();
         let mut buf = vec![0u8; 6];
         loop {
             f.read_exact(&mut buf[1..4])?;
@@ -33,21 +34,49 @@ impl Ips {
                 break;
             }
 
+            // Read length separately to avoid reading past the EOF
             f.read_exact(&mut buf[4..6])?;
 
             let address = BigEndian::read_u32(&buf[0..4]) as usize;
             let length = BigEndian::read_u16(&buf[4..6]) as usize;
 
-            // Create a buffer to hold the patch data
-            let mut data_buf = vec![0u8; length];
-            f.read_exact(&mut data_buf[0..length])?;
+            if length != 0 {
+                // Create a buffer to hold the patch data
+                let mut data_buf = vec![0u8; length];
+                f.read_exact(&mut data_buf[0..length])?;
 
-            // Replace bytes at address with patch data
-            for (buf_index, rom_address) in (address..address + 1).enumerate() {
-                rom[rom_address] = data_buf[buf_index];
+                // Replace bytes at address with patch data
+                for (buf_index, rom_address) in (address..address + length).enumerate() {
+                    if rom_address < rom_start_size {
+                        rom[rom_address] = data_buf[buf_index];
+                    } else {
+                        push_at(rom, rom_address, data_buf[buf_index]);
+                    }
+                }
+            } else {
+                // RLE Encoding
+                // Need to read two more bytes for the actual size
+                // One byte for the repeated byte
+
+                f.read_exact(&mut buf[1..4])?;
+                let length = BigEndian::read_u16(&buf[1..3]) as usize;
+                let byte = buf[3];
+
+                for rom_address in address..address + length {
+                    if rom_address < rom_start_size {
+                        rom[rom_address] = byte;
+                    } else {
+                        push_at(rom, rom_address, byte);
+                    }
+                }
             }
+        }
 
-            // TODO: RLE Encoding
+        fn push_at(v: &mut Vec<u8>, i: usize, b: u8) {
+            for j in v.len()..i {
+                v.push(0u8);
+            }
+            v.push(b);
         }
 
         Ok(())
