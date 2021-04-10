cdaa6dcad1fa67ca47803bc2d722799d91c6dd8d
Steven Casper <sebastiancasper3@gmail.com>
2021-04-09 21:17:47 -0400

    Add command line options
    This commit adds a Config object that is built based on arguments passed to the program
    Currently the only option is whehter or not to update the modlist.
    
    Additionally main() was refactored as follows: Modlist update code was extracted to
    a method; Modlist parsing extracted to method

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,63 @@ pub mod ui;
 use druid::{im::Vector, AppLauncher, LocalizedString, WindowDesc};
 
 use data::{AppState, Mod};
-use std::fs;
-use std::io::Write;
+use std::io::{Error, ErrorKind, Write};
+use std::{env, fs};
 
 const WINDOW_TITLE: &str = "bfbb_modloader";
 
+#[derive(Clone, Debug)]
+struct Config {
+    check_update: bool,
+}
+impl Config {
+    const DEFAULT_CONFIG: Config = Config { check_update: true };
+    const OPTION_UPDATE: &'static str = "--update";
+
+    fn new(args: &[String]) -> Self {
+        if args.len() < 3 {
+            return Config::DEFAULT_CONFIG.to_owned();
+        }
+
+        if &args[1] != Config::OPTION_UPDATE {
+            return Config::DEFAULT_CONFIG.to_owned();
+        }
+
+        match &args[2].parse::<bool>() {
+            Err(_) => Config::DEFAULT_CONFIG.to_owned(),
+            Ok(b) => Config { check_update: *b },
+        }
+    }
+}
+
 pub fn main() {
+    // Get config from command line args
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
+
     let main_window = WindowDesc::new(ui::ui_builder)
         .title(LocalizedString::new(WINDOW_TITLE).with_placeholder("BfBB Modloader"));
 
+    if config.check_update {
+        update_modlist();
+    }
+
+    //TODO: Error prompt when this fails
+    let modlist = match parse_modlist() {
+        Err(_) => {
+            println!("Failed to parse modlist");
+            Vec::new()
+        }
+        Ok(list) => list,
+    };
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(AppState::new(Vector::from(modlist)))
+        .expect("launch failed");
+}
+
+fn update_modlist() {
     match reqwest::blocking::get(data::URL_MODLIST) {
         Err(_) => println!("Failed to retrieve modslist from internet"), // TODO: Interent connectivity error
         Ok(response) => match response.text() {
@@ -30,23 +78,17 @@ pub fn main() {
             },
         },
     }
+}
 
+fn parse_modlist() -> std::io::Result<Vec<Mod>> {
     // TODO: Consider if saving the modlist locally is even necessary
     // Note: Keeping a local copy enables the app to still function even
     // if we fail to download latest version even though the user has a valid
     // internet connection
-    match fs::File::open(data::PATH_MODLIST) {
-        Err(_) => println! {"Failed to open file"}, // TODO: Failed to open file error
-        Ok(file) => {
-            match serde_json::from_reader::<_, Vec<Mod>>(file) {
-                Err(_) => println!("File corrupted"), // TODO: Failed to deserialize file error
-                Ok(modlist) => {
-                    AppLauncher::with_window(main_window)
-                        .use_simple_logger()
-                        .launch(AppState::new(Vector::from(modlist)))
-                        .expect("launch failed");
-                }
-            }
-        }
+    let file = fs::File::open(data::PATH_MODLIST)?;
+
+    match serde_json::from_reader::<_, Vec<Mod>>(file) {
+        Err(e) => Err(Error::new(ErrorKind::InvalidData, e)), //Failed to deserialize file
+        Ok(modlist) => Ok(modlist),
     }
 }
